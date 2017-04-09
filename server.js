97512786341854d72a4b799d0c76e4c815839e74
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-04-09 16:39:35 +0200

    Oops

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ var app = express()
 
 var port = 8008
 
-var client_id = '04ec33bc2f0544b4b06c9c78c251ff07'; // Your client id
-var client_secret = '595fe83b487d4924baef110f240d129b'; // Your secret
-var redirect_uri = 'http://localhost:8008/callback'; // Your redirect uri
-var auth_url = `https://accounts.spotify.com/authorize/?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=user-library-read`
-
-
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
